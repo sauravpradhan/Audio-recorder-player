c97a3abfb6f08030f8cea42abc43726918a34770
Saurav Pradhan <blogger.saurav@gmail.com>
2016-08-11 16:02:51 +0530

    Android Studio Support/Now creates Directory in External Storage

---
+++ README.md
@@ -0,0 +1,6 @@
+# Audio-recorder-player
+Record and Play audio in all possible AUDIOSOURCE, STREAM and MODE available in Android.
+
+The Audio Source, Stream and Mode in the application are as per API Level 21(All available till this API).
+The code has been tested in phone running Android L. 
+However it should work on phones running Android M too.
---
+++ gen/com/example/audiorecorder_player/BuildConfig.java
@@ -0,0 +1,16 @@
+<<<<<<< HEAD
+/*___Generated_by_IDEA___*/
+
+package com.example.audiorecorder_player;
+
+/* This stub is only used by the IDE. It is NOT the BuildConfig class actually packed into the APK */
+public final class BuildConfig {
+  public final static boolean DEBUG = Boolean.parseBoolean(null);
+=======
+/** Automatically generated file. DO NOT MODIFY */
+package com.example.audiorecorder_player;
+
+public final class BuildConfig {
+    public final static boolean DEBUG = true;
+>>>>>>> c7836d753cac7b91578e694a212bebff121c4876
+}
